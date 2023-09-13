9367b85d04ecfd7297e3dc942477a5241727174d
Youssef Emad <usf3md@gmail.com>
2023-09-13 16:49:38 +0300

    wave infront of content bug fixed

--- react-donut-shop/src/pages/SignUp/SignUp.tsx
+++ react-donut-shop/src/pages/SignUp/SignUp.tsx
@@ -5,7 +5,7 @@ const SignUp = () => {
     <>
       <div className={Style.parentContainer}>
         <div className={Style.backgroundImageContainer}></div>
-        <div className={Style.formContainer}>
+        <div className={Style.contentContainer}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 288 1440"
@@ -13,9 +13,9 @@ const SignUp = () => {
           >
             <path d="m0,1440l42.7-26.7c42.3-26.6,128.3-80.3,138.6-133.3,10.7-53.3-53.3-107-80-160-26.3-53.3-16.3-107,5.4-160,21.3-53.3,53.3-107,48-160-5.7-53.3-47.7-107-32-160,16.3-53.3,90.3-107,96-160,5.3-53.3-58.7-107-74.7-160-16-53.3,16-107,21.3-160,5.7-53.3-16.3-107-26.6-133L128,0h160v1440H0Z" />
           </svg>
-          <form action="" className={Style.form}>
+          <article className={Style.content}>
             <p>Your Content Here</p>
-          </form>
+          </article>
         </div>
       </div>
     </>
